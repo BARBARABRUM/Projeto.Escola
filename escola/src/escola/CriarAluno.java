ee79da671c4bf75c3c3fc7863f1e38566d253518
BARBARA <bbrumfpto@gmail.com>
2019-09-12 21:56:51 -0300

    Creating Aluno

---
+++ escola/src/escola/CriarAluno.java
@@ -0,0 +1,14 @@
+package escola;
+
+public class CriarAluno {
+
+	public static void main(String[] args) {
+	    Aluno aluno1 = new Aluno();
+	    aluno1.nome = "Juliana";
+	    aluno1.idade = 16;
+	    aluno1.nota1 = 7.0;
+	    aluno1.nota2 = 8.6;
+
+	}
+
+}
